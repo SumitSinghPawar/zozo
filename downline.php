9008c13e2ccf96a1bc7822784a41bb633c6fdc67
SumitSinghPawar <59958110+SumitSinghPawar@users.noreply.github.com>
2020-08-13 23:21:33 +0530

    Add files via upload

---
+++ downline.php
@@ -0,0 +1,163 @@
+<!DOCTYPE HTML>
+<!--
+	Alpha by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>downline</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<style type="text/css">
+			/* Styles for wrapping the search box */
+
+.main {
+    width: 50%;
+    margin: 50px auto;
+}
+
+/* Bootstrap 4 text input with search icon */
+
+.has-search .form-control {
+    padding-left: 2.375rem;
+}
+
+.has-search .form-control-feedback {
+    position: absolute;
+    z-index: 2;
+    display: block;
+    width: 2.375rem;
+    height: 2.375rem;
+    line-height: 2.375rem;
+    text-align: center;
+    pointer-events: none;
+    color: #aaa;
+}
+		</style>
+	</head>
+	<body class="is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<h1><a href="index.html"></a> </h1>
+					<nav id="nav" style="float: left;">
+						<ul style="text-align: center;float: left;">
+							<li><a href="index.php">Home</a></li>
+							<li><a href="account.php">Account</a></li>
+							<li><a href="Wallet.php">Wallet</a></li>
+							<li><a href="livechat.php">Live Chat</a></li>
+							<li><a href="downline.php">Downline</a></li>
+							<li><a href="profile.php">Profile Info</a></li>
+						</ul>
+					</nav>
+				</header>
+
+
+			<!-- Main -->
+				<section id="main" class="container">
+					<header>
+						<h2>Search Downline</h2>						
+					</header>
+				<div class="input-group">
+					<div class="box">
+						 <div class="form-group has-search">
+   						 	<span class="fa fa-search form-control-feedback"></span>
+    						<input type="text" class="form-control" placeholder="Search Downline By User ID">
+  						</div>
+					</div>
+				</div>
+					<section class="box">
+									<h1 style="text-align: center;font-size: 30px">Level 1 Downline</h1><br><br>
+									<div class="table-wrapper">
+										<table>
+											<thead>
+												<tr>
+													<th>S. No.</th>
+													<th>User ID</th>
+													<th>Name</th>
+													<th>Status</th>
+													<th>Join On</th>
+													<th>Activation On</th>
+													<th>Mobile Number</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												<tr>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												<tr>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												<tr>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												<tr>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+												</tr>
+												
+												
+												
+											</tbody>
+											
+										</table>
+									</div>
+
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						
+					</ul>
+					
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ withdraw.php
@@ -0,0 +1,85 @@
+<!DOCTYPE HTML>
+<!--
+	Alpha by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>withdraw</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<h1><a href="index.html"></a> </h1>
+					<nav id="nav" style="float: left;">
+						<ul style="text-align: center;float: left;">
+							<li><a href="index.php">Home</a></li>
+							<li><a href="account.php">Account</a></li>
+							<li><a href="Wallet.php">Wallet</a></li>
+							<li><a href="livechat.php">Live Chat</a></li>
+							<li><a href="downline.php">Downline</a></li>
+							<li><a href="profile.php">Profile Info</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Main -->
+				<section id="main" class="container">
+					<header>
+						<h2>Withdraw Your Money</h2>
+						
+					</header>
+					<div class="" style="width:920px;margin: auto;">
+					<div style="float: left;width: 450px" class="box">
+						<h1 style="font-size: 30px">Premium Plan Income</h1>
+						<br>
+						<form method="post" action="#">
+							<input type="text" name="amnt" id="amnt" value="" placeholder = "Enter Amount" required /><br>
+							<input type="submit" value="Withdraw" /><br><br><br><br>
+							<h1 style="font-size: 30px">Daily Task Income</h1><br>
+						</form>
+						<form method="post" action="#">
+							<input type="text" name="amnt" id="amnt" value="" placeholder = "Enter Amount" required /><br>
+							<input type="submit" value="Withdraw" /><br><br>
+						</form>
+					</div>
+					<div style="float: right;width: 450px" class="box">
+						<h1 style="font-size: 30px">Add Withdraw Info</h1>
+						<br>
+						<form method="post" action="#"><br>
+							<h1 style="font-size: 18px">Premium Plan Income Transaction History</h1><br>
+							<input type="submit" value="History" /><br><br><br>
+							<h1 style="font-size: 18px">Daily Task Income Transaction History</h1><br>
+							<input type="submit" value="History" /><br><br>
+						</form>
+					</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						
+					</ul>
+					
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
