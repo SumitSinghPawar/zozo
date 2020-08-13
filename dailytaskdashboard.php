39200cdc450e2694aa9c70dc85a3651401064855
SumitSinghPawar <59958110+SumitSinghPawar@users.noreply.github.com>
2020-08-13 23:52:25 +0530

    Add files via upload

---
+++ dailytaskdashboard.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML>
+<!--
+	Alpha by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Daily Task dash Board</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		
+	</head>
+	<body class="is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<h1><a href="index.html"></a> </h1>
+					<nav id="nav" style="float: left;">
+						<ul style="text-align: center;float: left;">
+							<li><a href="index.php">Home</a></li>
+							<li><a href="account.php">Account</a></li>
+							<li><a href="Wallet.php">Wallet</a></li>
+							<li><a href="livechat.php">Live Chat</a></li>
+							<li><a href="downline.php">Downline</a></li>
+							<li><a href="profile.php">Profile Info</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Main -->
+			<section>
+				<br><br>
+				
+	</div>
+			</section>
+			<h1 style="text-align: center;font-size: 30px">Daily Task Income Plan Dash Board</h1><br>
+							<!-- Table -->
+								<section class="box">
+									<br>
+									<div class="table-wrapper">
+										<table>
+											<thead>
+												<tr>
+													<th>Levels</th>
+													<th>Member</th>
+													<th>Active </th>
+													<th>Task Completed By</th>
+													<th>Daily Income</th>
+													<th>Total Income</th>
+													<th>Check Downline</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>level 1</td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td><a href="#" class="button alt small">Click</a></td>
+												</tr>
+												<tr>
+													<td>level 2</td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td><a href="#" class="button alt small">Click</a></td>
+												</tr>
+												<tr>
+													<td>level 3</td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td><a href="#" class="button alt small">Click</a></td>
+												</tr>
+												<tr>
+													<td>level 4</td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td><a href="#" class="button alt small">Click</a></td>
+												</tr>
+												<tr>
+													<td>level 5</td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td><a href="#" class="button alt small">Click</a></td>
+												</tr>
+												
+												
+												
+											</tbody>
+											
+										</table>
+									</div>
+
+									
+								</section>
+
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-12">
+
+							
+							<!-- Form -->
+								
+
+							<!-- Image -->
+								
+
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						
+					</ul>
+					<ul class="copyright">
+						
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
